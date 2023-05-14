5b59b33677c531a0057ac213978ed77341a0e8e7
Kourosh <kooroshoo@yahoo.com>
2023-05-14 20:02:13 +0200

    Added engine code and content examples

--- KEngine/Engine/main.cpp
+++ KEngine/Engine/main.cpp
@@ -1,21 +1,9 @@
-#include  "SFML/Graphics.hpp" // replace quotes with less than and greater than symbols
+#include <SFML/Graphics.hpp>
+
+#include "GameEngine.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Not Tetris");
-
-    sf::Event event;
-
-    while (window.isOpen()) {
-
-        while (window.pollEvent(event)) {
-
-            if (event.type == sf::Event::Closed) {
-
-                window.close();
-            }
-        }
-    }
-
-    return 0;
+    GameEngine g("assets.txt");
+    g.run();
 }
